751bafb27d679d89e2648a979597f3b3328a284d
Benjamin <erdle.benjamin@gmail.com>
2026-03-07 14:53:52 +0100

    :cc

--- flake.nix
+++ flake.nix
@@ -39,7 +39,6 @@
             ./hosts/PC.nix
             /etc/nixos/hardware-configuration.nix
             home-manager.nixosModules.home-manager
-	    nvf.nixosModules.default
           ];
         };
         Server = nixpkgs.lib.nixosSystem {
