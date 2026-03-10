eb8339ee0111a10d35be656b004ef515b0a7a8c6
Benjamin <erdle.benjamin@gmail.com>
2026-03-11 00:39:02 +0100

    added finalmouse support

--- modules/hardware/finalmouse.nix
+++ modules/hardware/finalmouse.nix
@@ -1,4 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
+let
+  finalmousePkgs = inputs.finalmouse.packages.x86_64-linux;
+in
 {
-  hardware.finalmouse.enable = true;
+  #imports = [ inputs.finalmouse.nixosModules.hardware.finalmouse ];
+  #hardware.finalmouse.enable = true;
+
+  environment.systemPackages = [ finalmousePkgs.xpanel ];
+  services.udev.packages = [ pkgs.finalmouse-udev-rules ];
 }
