756a48db8a31566a371155f2a5c67faa6eeeefe3
Benjamin <erdle.benjamin@gmail.com>
2026-04-08 21:22:24 +0200

    add wireguard vpn for server
    it actually works :)
    doesnt forward any traffic though - just makes server available via vpn
    connection

--- modules/server/vpn.nix
+++ modules/server/vpn.nix
@@ -1,55 +1,22 @@
-{ config, pkgs, lib, ... }:
+{ lib, ... }:
 {
-  networking.firewall.allowedTCPPorts = lib.mkAfter [ 51820 ];
-  
-  networking.useNetworkd = true;
+  networking.firewall.allowedUDPPorts = lib.mkAfter [ 51820 ];
 
-  systemd.network = {
-    enable = true;
-
-    networks."50-wg0" = {
-      matchConfig.Name = "wg0";
-
-      address = [ "10.0.0.1/24" ];
-      
-      networkConfig = {
-        # do not use IPMasquerade,
-        # unnecessary, causes problems with host ipv6
-        IPv4Forwarding = true;
-        IPv6Forwarding = true;
-      };
-    };
-
-    netdevs."50-wg0" = {
-      netdevConfig = {
-        Kind = "wireguard";
-        Name = "wg0";
-      };
-
-      wireguardConfig = {
-        ListenPort = 51820;
-
-        # ensure file is readable by `systemd-network` user
-        PrivateKeyFile = "/etc/wireguard/privatekey";
-
-        # To automatically create routes for everything in AllowedIPs,
-        # add RouteTable=main
-        RouteTable = "main";
-      };
-      
-      wireguardPeers = [
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      listenPort = 51820;
+      address = [
+        "10.10.10.1/32"
+      ];
+      privateKeyFile = "/srv/wireguard/privatekey";
+      peers = [
         {
-          PublicKey = "0WSxOMrb/0INi8TTn5IH65FXtMCUjojs1MkYy1TNsl4=";
-          AllowedIPs = [ "10.0.0.2/32" ];
+          publicKey = "0WSxOMrb/0INi8TTn5IH65FXtMCUjojs1MkYy1TNsl4=";
+          allowedIPs = [
+            "10.10.10.2/32"
+          ];
         }
       ];
     };
-  };
-  
-  networking.nat = {
-    enable = true;
-    enableIPv6 = true;
-    externalInterface = "ens6";
-    internalInterfaces = [ "wg0" ];
   };
 }
