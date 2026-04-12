4e1943d92a249ed553131dc4a91edb65e6762913
Benjamin <erdle.benjamin@gmail.com>
2026-04-12 05:21:39 +0200

    added restic secrets

--- modules/server/secrets/secrets.nix
+++ modules/server/secrets/secrets.nix
@@ -4,7 +4,11 @@ let
 
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMcw5t1oC4mYxTBLbD7Jd32JHmuURvkhh/e+KWae5Zft";
   systems = [ server ];
+
+  all = users ++ systems;
 in
 {
-  "secret1.age".publicKeys = users ++ systems;
+  "restic/repo_password.age".publicKeys = all;
+  "restic/repo_url.age".publicKeys = all;
+  "restic/repo_credentials.age".publicKeys = all;
 }
