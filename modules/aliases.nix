29dd067a2d44748ae7359dae7785afa2095cd14a
Benjamin <erdle.benjamin@gmail.com>
2026-04-15 20:32:22 +0200

    added aliases

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -2,6 +2,8 @@
 {
   environment.shellAliases = {
     ff = "fastfetch";
+    lg = "lazygit";
+    yz = "yazi";
     rs = "sudo nixos-rebuild switch --flake $HOME/NixOS --impure";
     rsu = "nix flake update --flake $HOME/NixOS && sudo nixos-rebuild switch --flake $HOME/NixOS --impure";
     gc = "nix-collect-garbage -d";
