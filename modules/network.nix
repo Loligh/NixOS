d3bd8709088c844a32e13c414fd648e68e73d8fc
Benjamin <erdle.benjamin@gmail.com>
2026-04-15 20:12:33 +0200

    removed custom dns server
    it made devices in the local network inaccessable through hostname

--- modules/network.nix
+++ modules/network.nix
@@ -2,10 +2,10 @@
 {
   networking = {
     networkmanager.enable = true;
-    networkmanager.dns = "none";
-    nameservers = [
-      "1.1.1.1"
-      "8.8.8.8"
-    ];
+    #networkmanager.dns = "none";
+    #nameservers = [
+    #  "1.1.1.1"
+    #  "8.8.8.8"
+    #];
   };
 }
