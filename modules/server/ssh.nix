31ca247b9a0cdecfdb8d1e5f615ff642128093dc
Benjamin <erdle.benjamin@gmail.com>
2026-04-16 18:17:57 +0200

    configured secure remote access to rooot (for remote rebuilds)

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -1,6 +1,18 @@
 { config, ... }:
 {
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgv0OuQSOVcYkSNDT2wmb/npBNgMzv+K0pVMFFzlQjS benjamin@PC"
+  ];
+
   services.openssh = {
     enable = true;
+    extraConfig = ''
+      Match user root
+        AllowTcpForwarding no
+        AllowAgentForwarding no
+        PasswordAuthentication no
+        KbdInteractiveAuthentication no
+        X11Forwarding no
+    '';
   };
 }
