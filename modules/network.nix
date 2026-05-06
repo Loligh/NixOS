dc6d0554545a6b71701081837982047e748b9212
Benjamin <erdle.benjamin@gmail.com>
2026-05-06 17:23:05 +0200

    added iperf to server to speedtest

--- modules/network.nix
+++ modules/network.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, lib, ... }:
 {
   networking = {
     networkmanager.enable = true;
@@ -8,4 +8,7 @@
     #  "8.8.8.8"
     #];
   };
+  networking.firewall.allowedUDPPorts = lib.mkAfter [ 5201 ];
+  networking.firewall.allowedTCPPorts = lib.mkAfter [ 5201 ];
+  environment.systemPackages = [ pkgs.iperf ];
 }
