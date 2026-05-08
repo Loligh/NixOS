75cf7b8ffda5706c9f6126ebc04944d9774d6fbe
Benjamin <erdle.benjamin@gmail.com>
2026-05-09 01:52:56 +0200

    added alias

--- modules/shell.nix
+++ modules/shell.nix
@@ -8,6 +8,7 @@
     lg = "lazygit";
     yz = "yazi";
     rs = "sudo nixos-rebuild switch --flake $HOME/NixOS";
+    rt = "sudo nixos-rebuild test --flake $HOME/NixOS";
     rsu = "nix flake update --flake $HOME/NixOS && sudo nixos-rebuild switch --flake $HOME/NixOS";
     gc = "nix-collect-garbage -d";
     try = "nix-shell -p";
