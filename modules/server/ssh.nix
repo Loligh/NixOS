3e4e4dad6275caaaaedeec32fd8310632f6c1a82
Benjamin <erdle.benjamin@gmail.com>
2026-05-08 23:06:26 +0200

    added pubkey for ssh

--- modules/server/ssh.nix
+++ modules/server/ssh.nix
@@ -1,11 +1,16 @@
-{ config, ... }:
+{ ... }:
 {
+  users.users.benjamin.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgv0OuQSOVcYkSNDT2wmb/npBNgMzv+K0pVMFFzlQjS benjamin@PC"
+  ];
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgv0OuQSOVcYkSNDT2wmb/npBNgMzv+K0pVMFFzlQjS benjamin@PC"
   ];
 
   services.openssh = {
     enable = true;
+    openFirewall = true; # temp
     extraConfig = ''
       Match user root
         AllowTcpForwarding no
