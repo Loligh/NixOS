070250a9d849fd67afadbeabc732f3bb7ba55551
Benjamin <erdle.benjamin@gmail.com>
2026-05-13 21:23:35 +0200

    put homemanger on right version + update

--- flake.nix
+++ flake.nix
@@ -3,11 +3,15 @@
 
   inputs = {
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-
-    home-manager = {
+    home-manager-unstable = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
+
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
     nvf = {
