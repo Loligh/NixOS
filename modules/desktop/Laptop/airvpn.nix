b4caab11ef7e4a9aeb1cbf67fd90bc73be8ceb2c
Benjamin <erdle.benjamin@gmail.com>
2026-06-19 15:41:18 +0200

    air vpn

--- modules/desktop/Laptop/airvpn.nix
+++ modules/desktop/Laptop/airvpn.nix
@@ -3,4 +3,20 @@
   environment.systemPackages = [
     pkgs.eddie
   ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      eddie = prev.eddie.overrideAttrs (old: {
+        postInstall = (old.postInstall or "") + ''
+          rm -f $out/lib/eddie/eddie-cli-elevated \
+                $out/lib/eddie/Lib.Platform.Linux.Native.so
+          cp $out/lib/eddie-ui/eddie-cli-elevated \
+             $out/lib/eddie/eddie-cli-elevated
+          cp $out/lib/eddie-ui/libLib.Platform.Linux.Native.so \
+             $out/lib/eddie/Lib.Platform.Linux.Native.so
+          chmod +x $out/lib/eddie/eddie-cli-elevated
+        '';
+      });
+    })
+  ];
 }
