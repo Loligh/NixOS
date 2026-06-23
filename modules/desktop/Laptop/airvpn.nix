71e2cf000f1775394fe41de9712e8fe70ff00ea5
Benjamin <erdle.benjamin@gmail.com>
2026-06-23 17:46:10 +0200

    airvpn fix removed
    got fixed upstream

--- modules/desktop/Laptop/airvpn.nix
+++ modules/desktop/Laptop/airvpn.nix
@@ -3,20 +3,4 @@
   environment.systemPackages = [
     pkgs.eddie
   ];
-
-  nixpkgs.overlays = [
-    (final: prev: {
-      eddie = prev.eddie.overrideAttrs (old: {
-        postInstall = (old.postInstall or "") + ''
-          rm -f $out/lib/eddie/eddie-cli-elevated \
-                $out/lib/eddie/Lib.Platform.Linux.Native.so
-          cp $out/lib/eddie-ui/eddie-cli-elevated \
-             $out/lib/eddie/eddie-cli-elevated
-          cp $out/lib/eddie-ui/libLib.Platform.Linux.Native.so \
-             $out/lib/eddie/Lib.Platform.Linux.Native.so
-          chmod +x $out/lib/eddie/eddie-cli-elevated
-        '';
-      });
-    })
-  ];
 }
