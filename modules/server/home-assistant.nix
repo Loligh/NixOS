4d202fd522ca8e52f67985f83e9cb5385ba9c3fc
Benjamin <erdle.benjamin@gmail.com>
2026-07-23 12:01:30 +0200

    ha 2

--- modules/server/home-assistant.nix
+++ modules/server/home-assistant.nix
@@ -1,14 +1,29 @@
-{ lib, ... }:
+{ config, lib, ... }:
 {
   networking.firewall.interfaces."wg-home".allowedTCPPorts = lib.mkAfter [ 8123 ];
 
   services.home-assistant = {
     enable = true;
-    config = {
-      http.server = {
-        server_host = "10.0.0.1";
-      };
-    };
+    config = null;
+    lovelaceConfig = null;
     configDir = "/srv/home-assistant";
+    extraComponents = [
+      "ssdp"
+      "usb"
+      "bluetooth"
+      "dhcp"
+      "stream"
+      "go2rtc"
+      "default_config"
+      "isal"
+      "mobile_app"
+
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+    ];
+
   };
 }
